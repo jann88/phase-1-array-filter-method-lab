e8ec9ee1e6f994fb7b02c755bb4ba8664c88d4cb
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-06-11 18:51:22 +0300

    commit tree a17e3bf9ed300e34179e41138055b1608462531a

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+// Code your solution herefunction
+function findMatching(source, sought) {
+    const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter(
+        (possibleMatch) =>
+        possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    );
+}
+
+function matchName(source, soughtName) {
+    return source.filter((record) => record.name === soughtName);
+}
